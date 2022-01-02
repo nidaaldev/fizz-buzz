1c4dbffb1830c7e516c4b640d482dceca7f19271
Nidal Dev <57320046+nidaaldev@users.noreply.github.com>
2022-01-02 11:01:18 +0100

    Add files via upload

---
+++ 03-app.js
@@ -0,0 +1,18 @@
+// Fizz buzz - 100 Números
+
+// 3 6 9 12... Fizz
+// 5 10 15... Buzz
+// 5 30 45... FizzBuzz!
+
+for (let i = 0; i <= 100; i++) {
+
+    if (i % 15 === 0) {
+        console.log(`FizzBuzz! ${i}`)    
+    } else if (i % 3 === 0) {
+        console.log(`Fizz ${i}`)
+        continue;
+    } else if (i % 5 === 0) {
+            console.log(`Buzz ${i}`)
+            continue;
+        }
+    }
